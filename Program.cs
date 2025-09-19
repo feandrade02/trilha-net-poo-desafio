d1533d34726a4c44de351f12716a97b57aef05ac
feandrade02 <fe.a.andrade@hotmail.com>
2025-09-19 14:44:42 -0300

    feat: adicionei as propriedades faltantes na classe Smartphone e completei seu construtor; completei as classes Nokia e iPhone com seus construtores e sobrescrevendo o método abstrato InstalarAplicativo; Adicionei testes em Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:\n");
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456789", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Numero = "987654321";
+Console.WriteLine($"Novo número do Nokia: {nokia.Numero}\n");
+
+Console.WriteLine("Smartphone iPhone:\n");
+Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI987654321", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
